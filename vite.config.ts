1c9590c8adb5740269c19e8807f077aefb8685c8
taiga1071101 <taigatukimi@gmail.com>
2025-02-09 23:27:35 +0900

    githubPagesのテスト

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/taiga1071101/React.js-kiso-4.git',
   plugins: [react()],
 })
