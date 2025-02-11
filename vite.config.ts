601bfb126e9cea7440f450606213e2991f89ea48
taiga1071101 <taigatukimi@gmail.com>
2025-02-11 11:07:59 +0900

    publicディレクトリ指定

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  publicDir: 'public', // publicディレクトリを指定（デフォルト）
   base: '/React.js-kiso-4',
   plugins: [react()],
 })
